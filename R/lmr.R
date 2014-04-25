869afbcba3eeef509063476de1e2a585634539d6
harry <harry.southworth@gmail.com>
2014-04-25 15:57:13 +0100

    Updating AICtab so that it now works ok

--- R/lmr.R
+++ R/lmr.R
@@ -47,6 +47,9 @@ ggqqplot <- function(o) {
 
 #' ggplot for rlm results
 #' @param data an object of class \code{rlm}.
+#' @param hist.scale A scaleing factor for bin widths in the histogram. The default bin width
+#'        will be \code{range(resid(data))/hist.scale)}. Defaults to \code{hist.scale=10},
+#'        which is smaler than the \code{ggplot} default of 30.
 #' @param ... Additional arguments passed to \code{ggplot}. Currently unused.
 #' @method ggplot rlm
 #' @importFrom gridExtra grid.arrange
@@ -84,6 +87,8 @@ ggplot.rlm <- function(data=NULL, hist.scale=10, ...){
 #' @param x An object of class 'rlm'.
 #' @param by A character string giving the name of a factor in \code{x}.
 #' @param jitter.width The amount of jittering to do. Defaults to \code{jitter.width = 0.1}.
+#' @param box Whether to do the boxplot. If the amount of data is small, it might be better
+#'        to just plot the jittered points. Defaults to \code{box=TRUE}.
 #' @param xlab Horizontal axis label. Defaults to \code{xlab=""}.
 #' @param ylab Vertical axis label. Defaults to \code{ylab="Scaled residuals"}.
 #' @param main Main title. Defaults to blank.
@@ -94,16 +99,20 @@ ggplot.rlm <- function(data=NULL, hist.scale=10, ...){
 #' @method boxplot rlm
 #' @export
 #' @importFrom graphics boxplot
-boxplot.rlm <- function(x, by, jitter.width=.1, xlab="", ylab="Scaled residuals",
-                        main="", ptcol="orange", boxcol="blue", boxfill="light blue", ...){
+boxplot.rlm <- function(x, by, jitter.width=.1, box=TRUE, xlab="", ylab="Scaled residuals",
+                        main="", size=3, ptcol="orange", boxcol="blue", boxfill="light blue", ...){
     trt <- x$data[, by]
     data <- data.frame(sr = resid(x) / x$s, trt=as.factor(trt))
+    if (box)
+      bp <- stat_boxplot(color=boxcol, fill=boxfill, alpha=.5, outlier.shape="")
+    else
+      bp <- NULL
     p <- ggplot(data, aes(trt, sr)) +
-           geom_jitter(color=ptcol, position=position_jitter(width=jitter.width)) +
-           stat_boxplot(color=boxcol, fill=boxfill, alpha=.5, outlier.shape="") +
+           geom_jitter(color=ptcol, size=size, alpha=.7, position=position_jitter(width=jitter.width)) +
            scale_x_discrete(xlab) +
            scale_y_continuous(ylab) +
-           ggtitle(main)
+           ggtitle(main) +
+           bp
     p
 }
 
