277a824b6d3f783aa58cae1acfc42b8efb32fe26
Harry <harry.southworth@gmail.com>
2014-09-18 14:42:26 +0100

    Adding roxygen R file to create help for lb and dm datasets

---
+++ R/dataHelp.R
@@ -0,0 +1,19 @@
+#' Data included in the margarita package
+#'
+#' @name lb
+#' @aliases dm
+#' @docType data
+#' @details The \code{liver} data, as found in the texmex (Southworth and Heffernan)
+#'          package, restructured so as to resemble CDISC SDTM datasets. The
+#'          lab values (ALT, AST, TBL and ALP) and dose groups are identical to
+#'          the \code{liver} dataset. The remaining values (age, sex, race and
+#'          so on) are simulated. The data are broken down into the lab data
+#'          (\code{lb}) and (largely fictional) `demography' data (\code{dm}).
+#'          The purpose is to illustrate the usage of some of the functions provided
+#'          by margarita to aid transformation of SDTM data into evm-ready analysis
+#'          data.
+#' @author Harry Southworth \email{harry.southworth@@gmail.com}
+#' @references  Harry Southworth and Janet E. Heffernan (2014). texmex: Statistical
+#'              modelling of extreme values. R package version 2.3.
+#' @keywords data
+NULL
